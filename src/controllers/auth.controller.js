64174ecc889b1559799d72cc95effd576798577a
Dewan Asifur Rahman <dasifrahman@gmail.com>
2025-01-10 17:52:51 +0600

    signup endpoint done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,50 @@
+import { generateToken } from "../lib/utils.js";
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-    res.send("signup route");
+    const { email, password, fullName } = req.body;
+    try {
+        if (!email || !password || !fullName) {
+            return res.status(400).json({ message: "All the fields are required" });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters" });
+        }
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            email,
+            fullName,
+            password: hashedPassword
+        });
+
+        if (newUser) {
+            // generate jwt token here
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            })
+        } else {
+            return res.status(400).json({ message: "Invalid user data" });
+        }
+    } catch (err) {
+        console.log("error in signup controller", err.message);
+        res.status(500).json({message: "Internal server error"});
+    }
 }
 
 export const login = async (req, res) => {
